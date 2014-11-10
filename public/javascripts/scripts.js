35b258a1f157cf8bb1afe7ed8ae3b99e96430960
maninigupta <manini.gupta@gmail.com>
2014-11-10 00:04:07 -0500

    get and display the hidden word when starting game of hangman

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,23 +1,3 @@
   function updateDisplay(data){
     $('.word-display').text(data.hiddenWord);
   }
-
-
-// on window load
-$(function(){
-  console.log("manini rockzzzz :)")
-
-  $('button.new-game').on('click', function(e){
-    $.ajax({
-      url: '/hangman/new', 
-      method: 'POST',
-      dataType: 'JSON',
-      success: function(data){
-        console.log(data);
-      }
-    });//end of ajax request
-  }); //end of new-game event listener
-
-
-
-});
